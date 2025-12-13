3f6f968fcbd572508c8b95766d49c664f82b6382
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-13 22:51:36 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,18 @@
+
 pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
+    }
+
+    plugins {
+        // Android Gradle Plugin
+        id("com.android.application") version "8.4.2"
+        id("org.jetbrains.kotlin.android") version "1.9.25"
+
+        // Google Services (Firebase)
+        id("com.google.gms.google-services") version "4.4.2"
     }
 }
 
