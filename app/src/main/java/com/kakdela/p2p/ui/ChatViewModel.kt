66e0a679c9f7441352b012e52e481f82d0dcad27
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-13 19:04:34 +0300

    Create ChatViewModel.kt

---
+++ app/src/main/java/com/kakdela/p2p/ui/ChatViewModel.kt
@@ -0,0 +1,24 @@
+package com.kakdela.p2p.ui
+
+import androidx.lifecycle.ViewModel
+import com.kakdela.p2p.data.ChatRepository
+import com.kakdela.p2p.data.Message
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class ChatViewModel : ViewModel() {
+
+    private val repo = ChatRepository()
+    private val _messages = MutableStateFlow<List<Message>>(emptyList())
+    val messages = _messages.asStateFlow()
+
+    fun start(chatId: String) {
+        repo.listenMessages(chatId) {
+            _messages.value = it
+        }
+    }
+
+    fun send(chatId: String, message: Message) {
+        repo.sendMessage(chatId, message)
+    }
+}
