e4e9461c5ae6c1bb213ccfd2ed8400cf33d09b23
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-15 10:06:49 +0300

    Update Message.kt

--- app/src/main/java/com/kakdela/p2p/data/Message.kt
+++ app/src/main/java/com/kakdela/p2p/data/Message.kt
@@ -2,5 +2,8 @@ package com.kakdela.p2p.data
 
 data class Message(
     val text: String = "",
-    val senderId: String = ""
+    val senderId: String = "",
+    val timestamp: Long = System.currentTimeMillis(),
+    val isDelivered: Boolean = false,
+    val isRead: Boolean = false
 )
