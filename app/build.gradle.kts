8971c2fc97db47d74c3f84ec97b3f4de5b262d8a
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-15 23:50:18 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.8" // стабильная версия для Compose 1.9.x
+        kotlinCompilerExtensionVersion = "1.5.12" // для Kotlin 1.9.0
     }
 
     compileOptions {
@@ -45,26 +45,22 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2025.12.00"))
 
     implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3:1.4.0")
+    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.compose.material:material-icons-extended:1.5.0")
     implementation("androidx.navigation:navigation-compose:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
 
     // Coil 3
     implementation("io.coil-kt.coil3:coil-compose:3.3.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.3.0")
 
-    // Firebase BoM
+    // Firebase
     implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
-
-    // Firebase основные модули (KTX встроен)
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
-
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.9.0")
 }
