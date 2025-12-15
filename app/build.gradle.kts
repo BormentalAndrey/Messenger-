807c0d0bb05803aa9fa1bc75fe11fd813d037535
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-15 11:39:18 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,24 +36,32 @@ android {
 
 dependencies {
 
+    // BOM
     implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
 
+    // Compose core
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended") // ← ВАЖНО
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("com.google.firebase:firebase-auth-ktx")
+
+    // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
+    // Lifecycle
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
 
-    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
-    implementation("com.google.firebase:firebase-auth")
+    // Firebase
+    implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore")
 
+    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.9.0")
 
+    // Debug
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
