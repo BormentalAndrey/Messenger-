0e510a235000a7814f343be7284dfba0b7738134
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-15 22:49:21 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,14 +7,10 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    // Строго запрещаем объявления репозиториев в build.gradle
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-
     repositories {
         google()
         mavenCentral()
-        // при необходимости добавьте JitPack:
-        // maven { url = uri("https://jitpack.io") }
     }
 }
 
