98a73ae7e9acc57fe3550a482e249f32f4836671
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-17 15:13:08 +0300

    Update DealsScreen.kt

--- app/src/main/java/com/kakdela/p2p/ui/DealsScreen.kt
+++ app/src/main/java/com/kakdela/p2p/ui/DealsScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
@@ -64,8 +63,10 @@ fun DealsScreen(navController: NavHostController) {
         LazyColumn(
             modifier = Modifier.fillMaxSize().background(Color.Black).padding(paddingValues)
         ) {
-            items(dealItems) { item ->
-                DealListItem(item = item, navController = navController, context = context)
+            dealItems.forEach { item ->
+                item {
+                    DealListItem(item = item, navController = navController, context = context)
+                }
             }
         }
     }
