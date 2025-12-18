db8d71a4c63d3bf78b54d3c21dbccd8f079da5ea
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-18 13:02:06 +0300

    Update MainActivity.kt

--- app/src/main/java/com/kakdela/p2p/MainActivity.kt
+++ app/src/main/java/com/kakdela/p2p/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kakdela.p2p
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.kakdela.p2p.ui.navigation.NavGraph
 import com.kakdela.p2p.ui.theme.Theme
@@ -10,10 +11,17 @@ import com.kakdela.p2p.ui.theme.Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // Разрешаем контенту заходить под системные панели (edge-to-edge)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        
         setContent {
+            // Оборачиваем всё приложение в неоновую тему
             Theme {
-                NavGraph(rememberNavController())
+                val navController = rememberNavController()
+                NavGraph(navController)
             }
         }
     }
 }
+
