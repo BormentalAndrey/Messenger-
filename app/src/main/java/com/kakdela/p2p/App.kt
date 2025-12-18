c240e60f9245dd0f97676553b9f198c70a3184f0
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-18 12:21:34 +0300

    Update App.kt

--- app/src/main/java/com/kakdela/p2p/App.kt
+++ app/src/main/java/com/kakdela/p2p/App.kt
@@ -1,11 +1,22 @@
 package com.kakdela.p2p
 
 import android.app.Application
+import android.content.Intent
 import com.google.firebase.FirebaseApp
+import com.kakdela.p2p.vpn.service.VpnService
 
 class App : Application() {
+
+    // включить VPN при старте
+    private val vpnAutoStart = true
+
     override fun onCreate() {
         super.onCreate()
+
         FirebaseApp.initializeApp(this)
+
+        if (vpnAutoStart) {
+            startService(Intent(this, VpnService::class.java))
+        }
     }
 }
