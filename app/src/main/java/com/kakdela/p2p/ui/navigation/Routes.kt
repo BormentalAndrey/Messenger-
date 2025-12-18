96b84ab192b2e50d3d305997b47573e1e1b18ebd
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-18 14:16:15 +0300

    Create Routes.kt

---
+++ app/src/main/java/com/kakdela/p2p/ui/navigation/Routes.kt
@@ -0,0 +1,20 @@
+package com.kakdela.p2p.ui.navigation
+
+object Routes {
+    const val SPLASH = "splash"
+    const val CHOICE = "choice"
+    const val AUTH_EMAIL = "auth_email"
+    const val AUTH_PHONE = "auth_phone"
+    const val CHATS = "chats"
+    const val CONTACTS = "contacts"
+    const val DEALS = "deals"
+    const val ENTERTAINMENT = "entertainment"
+    const val SETTINGS = "settings"
+    const val CALCULATOR = "calculator"
+    const val TIC_TAC_TOE = "tictactoe"
+    const val CHESS = "chess"
+    const val PACMAN = "pacman"
+    const val JEWELS = "jewels"
+    const val CHAT = "chat/{chatId}"
+    const val WEB_VIEW = "webview/{url}/{title}"
+}
