4ec96eec8f6ef709fcc4b5ff189883bbc4b3258f
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-19 19:38:16 +0300

    Update NetworkMonitor.kt

--- app/src/main/java/com/kakdela/p2p/vpn/core/NetworkMonitor.kt
+++ app/src/main/java/com/kakdela/p2p/vpn/core/NetworkMonitor.kt
@@ -1,21 +1,15 @@
 package com.kakdela.p2p.vpn.core
 
-import android.net.*
 import android.content.Context
-import com.kakdela.p2p.vpn.service.AutoRestartWorker
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 
-class NetworkMonitor(context: Context) {
-    private val cm = context.getSystemService(ConnectivityManager::class.java)
+class NetworkMonitor(private val context: Context) {
 
-    fun register() {
-        cm.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                AutoRestartWorker.schedule(cm.context)
-            }
-
-            override fun onLost(network: Network) {
-                AutoRestartWorker.schedule(cm.context)
-            }
-        })
+    fun isVpnActive(): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val nw = cm.activeNetwork ?: return false
+        val caps = cm.getNetworkCapabilities(nw) ?: return false
+        return caps.hasTransport(NetworkCapabilities.TRANSPORT_VPN)
     }
 }
