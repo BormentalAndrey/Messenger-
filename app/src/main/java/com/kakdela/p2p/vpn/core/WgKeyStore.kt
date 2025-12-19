67910f3f81bd4ce09b5181dae8386f17288111b9
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-19 19:05:39 +0300

    Update WgKeyStore.kt

--- app/src/main/java/com/kakdela/p2p/vpn/core/WgKeyStore.kt
+++ app/src/main/java/com/kakdela/p2p/vpn/core/WgKeyStore.kt
@@ -2,6 +2,7 @@ package com.kakdela.p2p.vpn.core
 
 import android.content.Context
 import android.util.Base64
+import com.wireguard.crypto.Key
 import java.security.SecureRandom
 
 class WgKeyStore(private val context: Context) {
@@ -11,11 +12,11 @@ class WgKeyStore(private val context: Context) {
         val existing = prefs.getString("private_key", null)
         if (existing != null) return existing
 
-        val random = ByteArray(32)
-        SecureRandom().nextBytes(random)
-        val key = Base64.encodeToString(random, Base64.NO_WRAP)
-
-        prefs.edit().putString("private_key", key).apply()
-        return key
+        val rnd = SecureRandom()
+        val keyBytes = ByteArray(Key.KEY_SIZE)
+        rnd.nextBytes(keyBytes)
+        val privateKey = Base64.encodeToString(keyBytes, Base64.NO_WRAP)
+        prefs.edit().putString("private_key", privateKey).apply()
+        return privateKey
     }
 }
