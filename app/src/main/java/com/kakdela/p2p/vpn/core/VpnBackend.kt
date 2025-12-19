9fe70dec26f80c3c247c641b2c68b6aae25746c2
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-19 19:39:37 +0300

    Update VpnBackend.kt

--- app/src/main/java/com/kakdela/p2p/vpn/core/VpnBackend.kt
+++ app/src/main/java/com/kakdela/p2p/vpn/core/VpnBackend.kt
@@ -1,30 +1,36 @@
 package com.kakdela.p2p.vpn.core
 
 import android.content.Context
-import android.net.VpnService
 import com.wireguard.android.backend.Tunnel
 import com.wireguard.android.backend.WgQuickBackend
-import com.wireguard.config.*
+import com.wireguard.config.Config
+import com.wireguard.config.InetAddress
+import com.wireguard.config.InetEndpoint
+import com.wireguard.config.InetNetwork
+import com.wireguard.config.Interface
+import com.wireguard.config.Peer
+import com.wireguard.crypto.Key
 
 class VpnBackend(private val context: Context) {
+
     private val backend by lazy { WgQuickBackend(context) }
 
     private val tunnel = object : Tunnel {
-        override fun getName(): String = "WARP"
-        override fun onStateChange(newState: Tunnel.State) {}
+        override fun getName() = "WARP"
+        override fun onStateChange(state: Tunnel.State) {}
     }
 
-    fun buildWarpConfig(pk: String): Config {
+    fun buildWarpConfig(privateKey: String): Config {
         val iface = Interface.Builder()
             .addAddress(InetNetwork.parse("172.16.0.2/32"))
-            .setPrivateKey(com.wireguard.crypto.Key.fromBase64(pk))
             .addDnsServer(java.net.InetAddress.getByName("1.1.1.1"))
+            .setPrivateKey(Key.fromBase64(privateKey))
             .build()
 
         val peer = Peer.Builder()
-            .setPublicKey(com.wireguard.crypto.Key.fromBase64("bmXOC+F1FxEMY9dyU9S47Vp00nU8NAs4W8uNP0R2D1s="))
-            .addAllowedIp(InetNetwork.parse("0.0.0.0/0"))
+            .setPublicKey(Key.fromBase64("bmXOC+F1FxEMY9dyU9S47Vp00nU8NAs4W8uNP0R2D1s="))
             .setEndpoint(InetEndpoint.parse("162.159.193.2:2408"))
+            .addAllowedIp(InetNetwork.parse("0.0.0.0/0"))
             .setPersistentKeepalive(25)
             .build()
 
@@ -34,18 +40,7 @@ class VpnBackend(private val context: Context) {
             .build()
     }
 
-    fun safeReconnect() {
-        try {
-            val pk = WgKeyStore(context).getPrivateKey()
-            up(buildWarpConfig(pk))
-        } catch (_: Throwable) {}
-    }
+    fun up(config: Config) = backend.setState(tunnel, Tunnel.State.UP, config)
 
-    fun up(config: Config) {
-        backend.setState(tunnel, Tunnel.State.UP, config)
-    }
-
-    fun down() {
-        backend.setState(tunnel, Tunnel.State.DOWN, null)
-    }
+    fun down() = backend.setState(tunnel, Tunnel.State.DOWN, null)
 }
