2eb9b9f5727a67ac532044c5e8608dbf62c5ad8f
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-19 20:54:00 +0300

    Update VpnBackend.kt

--- app/src/main/java/com/kakdela/p2p/vpn/core/VpnBackend.kt
+++ app/src/main/java/com/kakdela/p2p/vpn/core/VpnBackend.kt
@@ -1,7 +1,7 @@
 package com.kakdela.p2p.vpn.core
 
 import android.content.Context
-import com.wireguard.android.backend.GoBackend  // Рекомендую GoBackend (быстрее и стабильнее WgQuickBackend)
+import com.wireguard.android.backend.GoBackend
 import com.wireguard.android.backend.Tunnel
 import com.wireguard.config.Config
 import com.wireguard.config.Interface
@@ -18,7 +18,7 @@ class VpnBackend(private val context: Context) {
         override fun getName() = "WARP"
 
         override fun onStateChange(state: Tunnel.State) {
-            // Можно добавить логирование состояния для отладки
+            // Логирование состояния (опционально)
         }
     }
 
@@ -26,14 +26,14 @@ class VpnBackend(private val context: Context) {
         val iface = Interface.Builder()
             .addAddress(InetNetwork.parse("172.16.0.2/32"))
             .addDnsServer(InetAddress.getByName("1.1.1.1"))
-            .parsePrivateKey(privateKey)  // Правильный метод для приватного ключа
+            .parsePrivateKey(privateKey)  // Для приватного ключа
             .build()
 
         val peer = Peer.Builder()
-            .parsePublicKey("bmXOC+F1FxEMY9dyU9S47Vp00nU8NAs4W8uNP0R2D1s=")  // Правильный метод для публичного ключа
-            .endpoint(InetEndpoint.parse("162.159.193.2:2408"))
+            .parsePublicKey("bmXOC+F1FxEMY9dyU9S47Vp00nU8NAs4W8uNP0R2D1s=")  // Для публичного ключа
+            .setEndpoint(InetEndpoint.parse("162.159.193.2:2408"))  // Правильный метод: setEndpoint
             .addAllowedIp(InetNetwork.parse("0.0.0.0/0"))
-            .addAllowedIp(InetNetwork.parse("::/0"))  // Для поддержки IPv6 (опционально, но рекомендуется)
+            .addAllowedIp(InetNetwork.parse("::/0"))  // Для IPv6
             .persistentKeepalive(25)
             .build()
 
