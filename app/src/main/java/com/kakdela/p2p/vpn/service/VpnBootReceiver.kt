60ce36ef1b2e81eb77359f9108abe475ddb8633a
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-22 22:26:15 +0300

    Update VpnBootReceiver.kt

--- app/src/main/java/com/kakdela/p2p/vpn/service/VpnBootReceiver.kt
+++ app/src/main/java/com/kakdela/p2p/vpn/service/VpnBootReceiver.kt
@@ -1,25 +1,15 @@
 package com.kakdela.p2p.vpn.service
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
+import android.content.*
 import android.net.VpnService
 
 class VpnBootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
         if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
-            // Проверка: дано ли разрешение на VPN
-            val prepare = VpnService.prepare(context)
-            if (prepare == null) {
-                // Если prepare == null, значит разрешение ЕСТЬ -> запускаем сервис
-                val vpnIntent = Intent(context, com.kakdela.p2p.vpn.service.VpnService::class.java)
-                vpnIntent.action = com.kakdela.p2p.vpn.service.VpnService.ACTION_CONNECT
-                
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                    context.startForegroundService(vpnIntent)
-                } else {
-                    context.startService(vpnIntent)
-                }
+            if (VpnService.prepare(context) == null) {
+                context.startForegroundService(
+                    Intent(context, VpnService::class.java)
+                )
             }
         }
     }
