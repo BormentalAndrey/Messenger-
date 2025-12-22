32a0c833590528718f02d11d7499b09e1cc12764
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-22 22:27:32 +0300

    Update VpnBackend.kt

--- app/src/main/java/com/kakdela/p2p/vpn/core/VpnBackend.kt
+++ app/src/main/java/com/kakdela/p2p/vpn/core/VpnBackend.kt
@@ -1,40 +1,36 @@
 package com.kakdela.p2p.vpn.core
 
 import android.content.Context
+import com.kakdela.p2p.vpn.model.VpnServer
 import com.wireguard.android.backend.GoBackend
 import com.wireguard.android.backend.Tunnel
-import com.wireguard.config.Config
-import com.wireguard.config.Interface
-import com.wireguard.config.Peer
-import com.wireguard.config.InetEndpoint
-import com.wireguard.config.InetNetwork
+import com.wireguard.config.*
 import java.net.InetAddress
 
-class VpnBackend(private val context: Context) {
+class VpnBackend(context: Context) {
 
-    private val backend by lazy { GoBackend(context) }
+    private val backend = GoBackend(context)
 
     private val tunnel = object : Tunnel {
-        override fun getName() = "WARP"
-
-        override fun onStateChange(state: Tunnel.State) {
-            // Логирование (опционально)
-        }
+        override fun getName() = "KakdelaVPN"
+        override fun onStateChange(state: Tunnel.State) {}
     }
 
-    fun buildWarpConfig(privateKey: String): Config {
+    fun buildConfig(privateKey: String, server: VpnServer): Config {
+
         val iface = Interface.Builder()
+            .parsePrivateKey(privateKey)
             .addAddress(InetNetwork.parse("172.16.0.2/32"))
             .addDnsServer(InetAddress.getByName("1.1.1.1"))
-            .parsePrivateKey(privateKey)  // parsePrivateKey для приватного ключа
+            .addDnsServer(InetAddress.getByName("8.8.8.8"))
             .build()
 
         val peer = Peer.Builder()
-            .parsePublicKey("bmXOC+F1FxEMY9dyU9S47Vp00nU8NAs4W8uNP0R2D1s=")  // parsePublicKey
-            .setEndpoint(InetEndpoint.parse("162.159.193.2:2408"))  // setEndpoint
+            .parsePublicKey(server.publicKey)
+            .setEndpoint(InetEndpoint.parse("${server.host}:${server.port}"))
             .addAllowedIp(InetNetwork.parse("0.0.0.0/0"))
-            .addAllowedIp(InetNetwork.parse("::/0"))  // IPv6
-            .setPersistentKeepalive(25)  // Правильный метод: setPersistentKeepalive
+            .addAllowedIp(InetNetwork.parse("::/0"))
+            .setPersistentKeepalive(25)
             .build()
 
         return Config.Builder()
@@ -43,7 +39,11 @@ class VpnBackend(private val context: Context) {
             .build()
     }
 
-    fun up(config: Config) = backend.setState(tunnel, Tunnel.State.UP, config)
+    fun up(config: Config) {
+        backend.setState(tunnel, Tunnel.State.UP, config)
+    }
 
-    fun down() = backend.setState(tunnel, Tunnel.State.DOWN, null)
+    fun down() {
+        backend.setState(tunnel, Tunnel.State.DOWN, null)
+    }
 }
