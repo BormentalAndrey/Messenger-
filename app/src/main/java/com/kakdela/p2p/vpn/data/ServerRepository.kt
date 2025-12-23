9ae72e29ef56e6ad7fcbcbdbf0e9a58fbb73e1ac
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-23 22:31:14 +0300

    Update ServerRepository.kt

--- app/src/main/java/com/kakdela/p2p/vpn/data/ServerRepository.kt
+++ app/src/main/java/com/kakdela/p2p/vpn/data/ServerRepository.kt
@@ -4,14 +4,23 @@ import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.kakdela.p2p.vpn.model.VpnServer
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class ServerRepository(private val context: Context) {
-
+    
     fun load(): List<VpnServer> {
-        val json = context.assets.open("vpn/servers.json")
-            .bufferedReader().use { it.readText() }
-
-        val type = object : TypeToken<List<VpnServer>>() {}.type
-        return Gson().fromJson(json, type)
+        return try {
+            val inputStream = context.assets.open("vpn/servers.json")
+            val reader = BufferedReader(InputStreamReader(inputStream))
+            val jsonString = reader.use { it.readText() }
+            
+            val type = object : TypeToken<List<VpnServer>>() {}.type
+            Gson().fromJson(jsonString, type)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emptyList()
+        }
     }
 }
+
