14ec10b8b50bea5d887f8584828660ec0aeda037
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-29 22:01:03 +0300

    Create VpnActions.kt

---
+++ app/src/main/java/com/kakdela/p2p/vpn/VpnActions.kt
@@ -0,0 +1,6 @@
+package com.kakdela.p2p.vpn
+
+object VpnActions {
+    const val ACTION_CONNECT = "com.kakdela.p2p.ACTION_CONNECT"
+    const val ACTION_DISCONNECT = "com.kakdela.p2p.ACTION_DISCONNECT"
+}
