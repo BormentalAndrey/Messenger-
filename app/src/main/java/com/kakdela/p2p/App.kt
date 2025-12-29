58847c75ad3c3536dbd6706f52458beeff22a2aa
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2025-12-29 18:37:49 +0300

    Update App.kt

--- app/src/main/java/com/kakdela/p2p/App.kt
+++ app/src/main/java/com/kakdela/p2p/App.kt
@@ -1,26 +1,11 @@
 package com.kakdela.p2p
 
 import android.app.Application
-import android.content.Intent
-import android.os.Build
 import com.google.firebase.FirebaseApp
-import com.kakdela.p2p.vpn.service.KakdelaVpnService
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
-        
-        val intent = Intent(this, KakdelaVpnService::class.java)
-        try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startForegroundService(intent)
-            } else {
-                startService(intent)
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
     }
 }
-
