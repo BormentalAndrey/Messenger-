40a721ed1d70d4379b60d65fcb645af38a8e9c3a
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-03 00:42:29 +0300

    Delete app/src/main/java/com/kakdela/p2p/vpn/VpnActions.kt

--- app/src/main/java/com/kakdela/p2p/vpn/VpnActions.kt
+++
@@ -1,6 +0,0 @@
-package com.kakdela.p2p.vpn
-
-object VpnActions {
-    const val ACTION_CONNECT = "com.kakdela.p2p.ACTION_CONNECT"
-    const val ACTION_DISCONNECT = "com.kakdela.p2p.ACTION_DISCONNECT"
-}
