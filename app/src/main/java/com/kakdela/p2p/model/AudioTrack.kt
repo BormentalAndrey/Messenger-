ff32fb7bf9c52f5db1f1156e15ff69153888836a
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-03 23:40:28 +0300

    Update AudioTrack.kt

--- app/src/main/java/com/kakdela/p2p/model/AudioTrack.kt
+++ app/src/main/java/com/kakdela/p2p/model/AudioTrack.kt
@@ -6,7 +6,22 @@ data class AudioTrack(
     val id: Long,
     val title: String,
     val artist: String,
+    val albumTitle: String,
+    val trackNumber: Int,
     val duration: Long,
     val uri: Uri,
     val albumArt: Uri? = null
+)
+
+data class Album(
+    val id: Long,
+    val title: String,
+    val artist: String,
+    val albumArt: Uri?,
+    val tracks: List<AudioTrack>
+)
+
+data class Artist(
+    val name: String,
+    val tracks: List<AudioTrack>
 )
