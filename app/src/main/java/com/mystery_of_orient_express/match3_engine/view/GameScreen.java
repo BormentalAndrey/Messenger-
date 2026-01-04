ea96e62cbb075d2aef6b00c00ffa11a6e9fa9177
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-05 02:51:09 +0300

    Update GameScreen.java

--- app/src/main/java/com/mystery_of_orient_express/match3_engine/view/GameScreen.java
+++ app/src/main/java/com/mystery_of_orient_express/match3_engine/view/GameScreen.java
@@ -1,8 +1,10 @@
 package com.mystery_of_orient_express.match3_engine.view;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mystery_of_orient_express.match3_engine.model.IGameControl;
@@ -44,10 +46,25 @@ public class GameScreen extends ScreenAdapter implements IScreen, InputProcessor
 
     @Override
     public void render(float delta) {
+        // 1. Очистка экрана (обязательно для Android)
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // 2. Начало сессии отрисовки
+        batch.begin();
+
+        // Отрисовка фонового изображения
         if (assetManager.isLoaded("video.png")) {
             batch.draw(assetManager.get("video.png", Texture.class), 0, 0, screenWidth, screenHeight);
         }
-        for (IGameControl c : controls) c.render(delta, batch, assetManager);
+
+        // Отрисовка всех контроллеров (поле, счет и т.д.)
+        for (IGameControl c : controls) {
+            c.render(delta, batch, assetManager);
+        }
+
+        // 3. Завершение сессии отрисовки
+        batch.end();
     }
 
     @Override
@@ -56,7 +73,7 @@ public class GameScreen extends ScreenAdapter implements IScreen, InputProcessor
     @Override
     public boolean touchDown(int x, int y, int pointer, int button) {
         if (pointer != 0) return false;
-        int ly = screenHeight - y; // Инверсия Y
+        int ly = screenHeight - y;
         for (IGameControl c : controls) {
             InputProcessor ip = c.getInputProcessor();
             if (ip != null && ip.touchDown(x, ly, pointer, button)) {
@@ -82,18 +99,12 @@ public class GameScreen extends ScreenAdapter implements IScreen, InputProcessor
         return true;
     }
 
-    @Override
-    public boolean scrolled(float amountX, float amountY) { return false; }
-    @Override
-    public boolean mouseMoved(int x, int y) { return false; }
-    @Override
-    public boolean keyDown(int k) { return false; }
-    @Override
-    public boolean keyUp(int k) { return false; }
-    @Override
-    public boolean keyTyped(char c) { return false; }
-    @Override
-    public boolean touchCancelled(int x, int y, int p, int b) { 
+    @Override public boolean scrolled(float amountX, float amountY) { return false; }
+    @Override public boolean mouseMoved(int x, int y) { return false; }
+    @Override public boolean keyDown(int k) { return false; }
+    @Override public boolean keyUp(int k) { return false; }
+    @Override public boolean keyTyped(char c) { return false; }
+    @Override public boolean touchCancelled(int x, int y, int p, int b) { 
         currentProcessor = null; 
         return true; 
     }
