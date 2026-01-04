c8a96a71e88bb97635ead3cb7e8f5afbe764577a
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-05 02:18:24 +0300

    Update DisappearAnimation.java

--- app/src/main/java/com/mystery_of_orient_express/match3_engine/controller/DisappearAnimation.java
+++ app/src/main/java/com/mystery_of_orient_express/match3_engine/controller/DisappearAnimation.java
@@ -8,12 +8,15 @@ import com.mystery_of_orient_express.match3_engine.model.IAnimation;
 public class DisappearAnimation implements IAnimation {
     private static final float totalDuration = 0.1666666f;
     private static final float totalDurationInv = 6.0f;
+    
+    public Set<CellObject> gems; // Сделано public для доступа из View
     private IAnimationHandler handler;
     private CellObject[] gemsArray;
     private float currentDuration;
     private float gemSize;
 
     public DisappearAnimation(Set<CellObject> gems, float gemSize, IAnimationHandler handler) {
+        this.gems = gems;
         this.handler = handler;
         this.gemsArray = gems.toArray(new CellObject[0]);
         for (CellObject gem : this.gemsArray) {
@@ -32,7 +35,7 @@ public class DisappearAnimation implements IAnimation {
             }
             this.handler.onComplete(this);
         } else {
-            float newSize = this.gemSize * (1 - this.currentDuration * totalDurationInv);
+            float newSize = this.gemSize * (1.0f - this.currentDuration * totalDurationInv);
             for (CellObject gem : this.gemsArray) {
                 GameObject go = (GameObject) gem;
                 go.sizeY = go.sizeX = newSize;
