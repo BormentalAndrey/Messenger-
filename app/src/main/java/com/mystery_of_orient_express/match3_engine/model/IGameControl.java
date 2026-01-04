98fe9150f6c5822bc8928cd6be56a70a522bf88b
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-05 02:05:11 +0300

    Update IGameControl.java

--- app/src/main/java/com/mystery_of_orient_express/match3_engine/model/IGameControl.java
+++ app/src/main/java/com/mystery_of_orient_express/match3_engine/model/IGameControl.java
@@ -4,10 +4,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.InputProcessor;
 
-public interface IGameControl
-{
-	public void load(AssetManager assetManager);
-	public void resize(int x, int y, int width, int height);
-	public void render(float delta, SpriteBatch batch, AssetManager assetManager);
-	public InputProcessor getInputProcessor();
+public interface IGameControl {
+    void load(AssetManager assetManager);
+    void resize(int x, int y, int width, int height);
+    // Добавлен delta для плавной анимации
+    void render(float delta, SpriteBatch batch, AssetManager assetManager);
+    InputProcessor getInputProcessor();
 }
+
