0e17d21426cc09cd8fd4d44e2c8ac5307327b4bf
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-05 02:10:12 +0300

    Update IAnimationHandler.java

--- app/src/main/java/com/mystery_of_orient_express/match3_engine/controller/IAnimationHandler.java
+++ app/src/main/java/com/mystery_of_orient_express/match3_engine/controller/IAnimationHandler.java
@@ -2,7 +2,7 @@ package com.mystery_of_orient_express.match3_engine.controller;
 
 import com.mystery_of_orient_express.match3_engine.model.IAnimation;
 
-public interface IAnimationHandler
-{
-	public void onComplete(IAnimation animation);
+public interface IAnimationHandler {
+    void onComplete(IAnimation animation);
 }
+
