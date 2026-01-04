2c58e1ece3f672fa689918d07ecb51e51ab99aba
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-04 23:17:07 +0300

    Update GameScreen.java

--- app/src/main/java/com/mystery_of_orient_express/match3_engine/view/GameScreen.java
+++ app/src/main/java/com/mystery_of_orient_express/match3_engine/view/GameScreen.java
@@ -12,140 +12,142 @@ import com.mystery_of_orient_express.match3_engine.model.IGameControl;
 
 public class GameScreen extends ScreenAdapter implements IScreen, InputProcessor
 {
-	private int screenWidth;
-	private int screenHeight;
-	private int gameFieldSize;
-	private SpriteBatch batch;
-	private AssetManager assetManager;
-	private List<IGameControl> controls;
-	private InputProcessor inputProcessor = null;
-	
-	public GameScreen(SpriteBatch batch)
-	{
-		this.batch = batch;
-		this.controls = new ArrayList<IGameControl>();
-		ScoreControl scoreControl = new ScoreControl();
-		this.controls.add(new GameFieldControl(scoreControl, 8));
-		this.controls.add(scoreControl);
-	}
+    private int screenWidth;
+    private int screenHeight;
+    private int gameFieldSize;
+    private SpriteBatch batch;
+    private AssetManager assetManager;
+    private List<IGameControl> controls;
+    private InputProcessor inputProcessor = null;
 
-	@Override
-	public void load(AssetManager assetManager)
-	{
-		this.assetManager = assetManager;
-		assetManager.load("video.png", Texture.class);
-		for (IGameControl control: this.controls)
-		{
-			control.load(this.assetManager);
-		}
-	}
+    public GameScreen(SpriteBatch batch)
+    {
+        this.batch = batch;
+        this.controls = new ArrayList<>();
+        ScoreControl scoreControl = new ScoreControl();
+        this.controls.add(new GameFieldControl(scoreControl, 8));
+        this.controls.add(scoreControl);
+    }
 
-	@Override
-	public void resize(int width, int height)
-	{
-		this.screenWidth = width;
-		this.screenHeight = height;
-		this.gameFieldSize = Math.min(this.screenWidth, (int)(0.9f * this.screenHeight));
-		for (IGameControl control: this.controls)
-		{
-			if (control instanceof ScoreControl)
-			{
-				control.resize(0, this.gameFieldSize, this.screenWidth, this.screenHeight - this.gameFieldSize);
-			}
-			if (control instanceof GameFieldControl)
-			{
-				control.resize((width - this.gameFieldSize) / 2, 0, this.gameFieldSize, this.gameFieldSize);
-			}
-		}
-	}
+    @Override
+    public void load(AssetManager assetManager)
+    {
+        this.assetManager = assetManager;
+        assetManager.load("video.png", Texture.class);
+        for (IGameControl control : this.controls)
+        {
+            control.load(this.assetManager);
+        }
+    }
 
-	@Override
-	public InputProcessor getInputProcessor()
-	{
-		return this;
-	}
+    @Override
+    public void resize(int width, int height)
+    {
+        this.screenWidth = width;
+        this.screenHeight = height;
+        this.gameFieldSize = Math.min(this.screenWidth, (int)(0.9f * this.screenHeight));
 
-	@Override
-	public void render(float delta)
-	{
-		Texture image = assetManager.get("video.png");;
-		batch.draw(image, 0, 0, this.screenWidth, this.screenHeight);
-		for (IGameControl control: this.controls)
-		{
-			control.render(delta, this.batch, this.assetManager);
-		}
-	}
-	
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button)
-	{
-		if (pointer != 0)
-			return false;
+        for (IGameControl control : this.controls)
+        {
+            if (control instanceof ScoreControl)
+            {
+                control.resize(0, this.gameFieldSize, this.screenWidth, this.screenHeight - this.gameFieldSize);
+            }
+            if (control instanceof GameFieldControl)
+            {
+                control.resize((width - this.gameFieldSize) / 2, 0, this.gameFieldSize, this.gameFieldSize);
+            }
+        }
+    }
 
-		for (IGameControl control: this.controls)
-		{
-			InputProcessor inputProcessor = control.getInputProcessor();
-			if (inputProcessor != null && inputProcessor.touchDown(screenX, this.screenHeight - screenY, pointer, button))
-			{
-				this.inputProcessor = control.getInputProcessor();
-				return true;
-			}
-		}
+    @Override
+    public InputProcessor getInputProcessor()
+    {
+        return this;
+    }
 
-		return false;
-	}
+    @Override
+    public void render(float delta)
+    {
+        Texture image = assetManager.get("video.png");
+        batch.draw(image, 0, 0, this.screenWidth, this.screenHeight);
 
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button)
-	{
-		if (pointer != 0 || this.inputProcessor == null)
-			return false;
+        for (IGameControl control : this.controls)
+        {
+            control.render(delta, this.batch, this.assetManager);
+        }
+    }
 
-		if (this.inputProcessor.touchUp(screenX, this.screenHeight - screenY, pointer, button))
-			this.inputProcessor = null;
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button)
+    {
+        if (pointer != 0)
+            return false;
 
-		return true;
-	}
+        for (IGameControl control : this.controls)
+        {
+            InputProcessor ip = control.getInputProcessor();
+            if (ip != null && ip.touchDown(screenX, this.screenHeight - screenY, pointer, button))
+            {
+                this.inputProcessor = ip;
+                return true;
+            }
+        }
+        return false;
+    }
 
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer)
-	{
-		if (pointer != 0 || this.inputProcessor == null)
-			return false;
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button)
+    {
+        if (pointer != 0 || this.inputProcessor == null)
+            return false;
 
-		if (this.inputProcessor.touchDragged(screenX, this.screenHeight - screenY, pointer))
-			this.inputProcessor = null;
+        if (this.inputProcessor.touchUp(screenX, this.screenHeight - screenY, pointer, button))
+            this.inputProcessor = null;
 
-		return true;
-	}
+        return true;
+    }
 
-	@Override
-	public boolean mouseMoved(int screenX, int screenY)
-	{
-		return false;
-	}
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer)
+    {
+        if (pointer != 0 || this.inputProcessor == null)
+            return false;
 
-	@Override
-	public boolean scrolled(int amount)
-	{
-		return false;
-	}
+        if (this.inputProcessor.touchDragged(screenX, this.screenHeight - screenY, pointer))
+            this.inputProcessor = null;
 
-	@Override
-	public boolean keyDown(int keycode)
-	{
-		return false;
-	}
+        return true;
+    }
 
-	@Override
-	public boolean keyUp(int keycode)
-	{
-		return false;
-	}
+    @Override
+    public boolean mouseMoved(int screenX, int screenY)
+    {
+        return false;
+    }
 
-	@Override
-	public boolean keyTyped(char character)
-	{
-		return false;
-	}
+    // ✅ НОВАЯ СИГНАТУРА libGDX 1.12+
+    @Override
+    public boolean scrolled(float amountX, float amountY)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character)
+    {
+        return false;
+    }
 }
