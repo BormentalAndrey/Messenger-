ba53a968ab9e1bad447ae7a62582998baf38921b
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-05 02:01:51 +0300

    Update SplashScreen.java

--- app/src/main/java/com/mystery_of_orient_express/match3_engine/view/SplashScreen.java
+++ app/src/main/java/com/mystery_of_orient_express/match3_engine/view/SplashScreen.java
@@ -1,56 +1,49 @@
 package com.mystery_of_orient_express.match3_engine.view;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class SplashScreen extends ScreenAdapter implements IScreen
-{
-	private int screenWidth;
-	private int screenHeight;
-	private SpriteBatch batch;
-	private AssetManager assetManager;
-	private Texture splashImage;
-	private Texture progressImage;
+public class SplashScreen extends ScreenAdapter implements IScreen {
+    private int screenWidth, screenHeight;
+    private SpriteBatch batch;
+    private AssetManager assetManager;
 
-	public SplashScreen(SpriteBatch batch)
-	{
-		this.batch = batch;
-	}
+    public SplashScreen(SpriteBatch batch) { this.batch = batch; }
 
-	@Override
-	public void resize(int width, int height)
-	{
-		this.screenWidth = width;
-		this.screenHeight = height;
-	}
+    @Override
+    public void resize(int width, int height) {
+        this.screenWidth = width;
+        this.screenHeight = height;
+    }
 
-	@Override
-	public void load(AssetManager assetManager)
-	{
-		this.assetManager = assetManager;
-		this.assetManager.load("splash.png", Texture.class);
-		this.assetManager.load("loaded.png", Texture.class);
-		this.assetManager.finishLoading();
-		this.splashImage = this.assetManager.get("splash.png", Texture.class);
-		this.progressImage = this.assetManager.get("loaded.png", Texture.class);
-	}
+    @Override
+    public void load(AssetManager assetManager) {
+        this.assetManager = assetManager;
+        assetManager.load("splash.png", Texture.class);
+        assetManager.load("loaded.png", Texture.class);
+        assetManager.finishLoading();
+    }
 
-	@Override
-	public InputProcessor getInputProcessor()
-	{
-		return null;
-	}
+    @Override
+    public void render(float delta) {
+        float progress = assetManager.getProgress();
+        Texture splash = assetManager.get("splash.png", Texture.class);
+        Texture loaded = assetManager.get("loaded.png", Texture.class);
 
-	@Override
-	public void render(float delta)
-	{
-		float progress = this.assetManager.getProgress();
-		this.batch.draw(this.splashImage, 0, 0, this.screenWidth, this.screenHeight);
-		//this.batch.draw(this.progressImage, 0, 0, 100, 100);
-		this.batch.draw(this.progressImage, 0.0f, 0.0f, this.screenWidth * progress, (float)this.screenHeight,
-			0, 0, (int)(this.progressImage.getWidth() * progress), this.progressImage.getHeight(), false, false);
-	}
+        batch.begin();
+        batch.draw(splash, 0, 0, screenWidth, screenHeight);
+        // Рисуем прогресс-бар снизу
+        batch.draw(loaded, 0, 0, screenWidth * progress, screenHeight * 0.05f, 
+                   0, 0, (int)(loaded.getWidth() * progress), loaded.getHeight(), false, false);
+        batch.end();
+    }
+
+    @Override
+    public InputProcessor getInputProcessor() { return null; }
 }
+
