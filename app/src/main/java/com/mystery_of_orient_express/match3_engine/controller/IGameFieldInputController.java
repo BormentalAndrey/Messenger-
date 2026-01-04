6a77d3f6234d51b206f12dd2bde9e6e5344796be
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-05 02:09:26 +0300

    Update IGameFieldInputController.java

--- app/src/main/java/com/mystery_of_orient_express/match3_engine/controller/IGameFieldInputController.java
+++ app/src/main/java/com/mystery_of_orient_express/match3_engine/controller/IGameFieldInputController.java
@@ -1,8 +1,8 @@
 package com.mystery_of_orient_express.match3_engine.controller;
 
-public interface IGameFieldInputController
-{
-	public boolean canMove();
-	public boolean checkIndex(int index);
-	public void swap(int i1, int j1, int i2, int j2);
+public interface IGameFieldInputController {
+    boolean canMove();
+    boolean checkIndex(int index);
+    void swap(int i1, int j1, int i2, int j2);
 }
+
