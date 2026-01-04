cc6c236be252db9644c3e1b0af1a005da714e58f
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-05 02:04:30 +0300

    Update GameObject.java

--- app/src/main/java/com/mystery_of_orient_express/match3_engine/model/GameObject.java
+++ app/src/main/java/com/mystery_of_orient_express/match3_engine/model/GameObject.java
@@ -1,18 +1,16 @@
 package com.mystery_of_orient_express.match3_engine.model;
 
-public class GameObject extends CellObject
-{
-	public float posX = 0;
-	public float posY = 0;
-	public float sizeX = 0;
-	public float sizeY = 0;
+public class GameObject extends CellObject {
+    public float posX, posY;
+    public float sizeX, sizeY;
 
-	public GameObject(int kind, float posX, float posY, float sizeX, float sizeY)
-	{
-		this.kind = kind;
-		this.posX = posX;
-		this.posY = posY;
-		this.sizeX = sizeX;
-		this.sizeY = sizeY;
-	}
+    public GameObject(int kind, float posX, float posY, float sizeX, float sizeY) {
+        this.kind = kind;
+        this.posX = posX;
+        this.posY = posY;
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.activity = -1; // Инициализация как спокойного объекта
+    }
 }
+
