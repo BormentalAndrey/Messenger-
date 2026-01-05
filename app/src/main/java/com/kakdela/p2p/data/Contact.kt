3c1fb4c367ab5982b7ff2d49e34e9eff70f0057b
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-06 00:49:59 +0300

    Update Contact.kt

--- app/src/main/java/com/kakdela/p2p/data/Contact.kt
+++ app/src/main/java/com/kakdela/p2p/data/Contact.kt
@@ -2,7 +2,9 @@ package com.kakdela.p2p.data
 
 data class AppContact(
     val name: String,
-    val phoneNumber: String,        // Нормализованный номер (11 цифр, начинается с 7)
-    val uid: String? = null,        // null = не зарегистрирован в приложении
+    val phoneNumber: String,
+    val uid: String? = null,
+    val publicKey: String? = null, // Ключ для шифрования сообщений этому юзеру
     val isRegistered: Boolean = false
 )
+
