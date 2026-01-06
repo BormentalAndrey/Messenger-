1fe48a95f3fee7c43fa722f96652cd489827300d
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-06 16:18:34 +0300

    Update Message.kt

--- app/src/main/java/com/kakdela/p2p/data/Message.kt
+++ app/src/main/java/com/kakdela/p2p/data/Message.kt
@@ -2,20 +2,21 @@ package com.kakdela.p2p.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 @Entity(tableName = "messages")
 data class Message(
-    @PrimaryKey var id: String = "",
+    @PrimaryKey var id: String = "", // Firebase ID как основной ключ
     val text: String = "",
     val senderId: String = "",
     val timestamp: Long = System.currentTimeMillis(),
     val type: MessageType = MessageType.TEXT,
     val fileUrl: String? = null,
     val fileName: String? = null,
-    val fileSize: Long = 0,
     val durationSeconds: Int = 0,
     val isP2P: Boolean = false,
-    var isMe: Boolean = false // Теперь это изменяемое поле для маппинга
+    var isMe: Boolean = false 
 )
 
 enum class MessageType {
