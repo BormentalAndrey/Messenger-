e5025547b4ce8798c2451466ae72629f4ce40662
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-07 03:08:14 +0300

    Delete app/src/main/java/com/kakdela/p2p/data/ContactModel.kt

--- app/src/main/java/com/kakdela/p2p/data/ContactModel.kt
+++
@@ -1,9 +0,0 @@
-package com.kakdela.p2p.data
-
-data class ContactModel(
-    val name: String,
-    val phoneNumber: String,
-    val isRegistered: Boolean = false,
-    val userId: String? = null
-)
-
