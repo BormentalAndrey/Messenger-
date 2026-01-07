e91c949baea005ab85377f0309b551eda2b05970
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-07 06:40:19 +0300

    Update AppContact.kt

--- app/src/main/java/com/kakdela/p2p/data/AppContact.kt
+++ app/src/main/java/com/kakdela/p2p/data/AppContact.kt
@@ -2,11 +2,6 @@ package com.kakdela.p2p.data
 
 /**
  * Единая модель контакта для P2P мессенджера.
- * @param name Имя из телефонной книги
- * @param phoneNumber Нормализованный номер (например, 79991234567)
- * @param publicKey Публичный ключ, найденный в DHT (используется как ID)
- * @param isRegistered Указывает, найден ли пользователь в P2P сети
- * @param lastKnownIp Актуальный IP адрес для отправки UDP пакетов
  */
 data class AppContact(
     val name: String,
@@ -15,3 +10,4 @@ data class AppContact(
     val isRegistered: Boolean = false,
     val lastKnownIp: String? = null
 )
+
