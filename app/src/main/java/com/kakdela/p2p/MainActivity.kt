b8db85c94fff036742454ed8aa4529737b05103f
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-07 03:14:23 +0300

    Update MainActivity.kt

--- app/src/main/java/com/kakdela/p2p/MainActivity.kt
+++ app/src/main/java/com/kakdela/p2p/MainActivity.kt
@@ -4,19 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
+import com.kakdela.p2p.data.IdentityRepository
 import com.kakdela.p2p.ui.navigation.NavGraph
 import com.kakdela.p2p.ui.theme.KakdelaTheme
 
 class MainActivity : ComponentActivity() {
 
+    // Инициализируем репозиторий здесь, чтобы P2P-стек жил вместе с приложением
+    private lateinit var identityRepository: IdentityRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        identityRepository = IdentityRepository(applicationContext)
 
         setContent {
             KakdelaTheme {
                 val navController = rememberNavController()
-                NavGraph(navController)
+                // Передаем identityRepository в NavGraph, чтобы экраны могли его использовать
+                NavGraph(navController, identityRepository)
             }
         }
     }
 }
+
