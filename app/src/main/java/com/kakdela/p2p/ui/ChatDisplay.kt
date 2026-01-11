0192b159ebe9829cd8f25d7401b5cc907e74133e
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-11 06:16:07 +0300

    Delete app/src/main/java/com/kakdela/p2p/ui/ChatDisplay.kt

--- app/src/main/java/com/kakdela/p2p/ui/ChatDisplay.kt
+++
@@ -1,9 +0,0 @@
-package com.kakdela.p2p.ui
-
-data class ChatDisplay(
-    val id: String,         // Hash-ID собеседника
-    val title: String,      // Изначально номер телефона
-    val lastMessage: String,
-    val time: String,
-    val unreadCount: Int = 0
-)
