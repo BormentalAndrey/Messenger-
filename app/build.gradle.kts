dfe15591960f216be983b45cbe29cb2746366f60
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-11 23:36:41 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -117,6 +117,7 @@ android {
 
 dependencies {
     // Core & Lifecycle
+    implementation "com.squareup.okhttp3:logging-interceptor:4.12.0"
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2") // ДОБАВЛЕНО для работы с viewModel() в NavGraph
