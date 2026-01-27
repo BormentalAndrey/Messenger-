856a4f614477e1a21bc23d7135e326465e510178
BormentalAndrey <glavnaya.kompaniya@mail.ru>
2026-01-27 21:13:01 +0300

    Update TermuxInstaller.java

--- app/src/main/java/com/termux/app/TermuxInstaller.java
+++ app/src/main/java/com/termux/app/TermuxInstaller.java
@@ -11,17 +11,17 @@ import android.util.Pair;
 import android.view.WindowManager;
 
 import com.kakdela.p2p.R;
-import com.kakdela.p2p.shared.file.FileUtils;
-import com.kakdela.p2p.shared.termux.crash.TermuxCrashUtils;
-import com.kakdela.p2p.shared.termux.file.TermuxFileUtils;
-import com.kakdela.p2p.shared.interact.MessageDialogUtils;
-import com.kakdela.p2p.shared.logger.Logger;
-import com.kakdela.p2p.shared.markdown.MarkdownUtils;
-import com.kakdela.p2p.shared.errors.Error;
-import com.kakdela.p2p.shared.android.PackageUtils;
-import com.kakdela.p2p.shared.termux.TermuxConstants;
-import com.kakdela.p2p.shared.termux.TermuxUtils;
-import com.kakdela.p2p.shared.termux.shell.command.environment.TermuxShellEnvironment;
+import com.termux.shared.file.FileUtils;
+import com.termux.shared.termux.crash.TermuxCrashUtils;
+import com.termuxshared.termux.file.TermuxFileUtils;
+import com.termux.shared.interact.MessageDialogUtils;
+import com.termux.shared.logger.Logger;
+import com.termux.shared.markdown.MarkdownUtils;
+import com.termux.shared.errors.Error;
+import com.termux.shared.android.PackageUtils;
+import com.termux.shared.termux.TermuxConstants;
+import com.termux.shared.termux.TermuxUtils;
+import com.termux.shared.termux.shell.command.environment.TermuxShellEnvironment;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
@@ -33,10 +33,10 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-import static com.kakdela.p2p.shared.termux.TermuxConstants.TERMUX_PREFIX_DIR;
-import static com.kakdela.p2p.shared.termux.TermuxConstants.TERMUX_PREFIX_DIR_PATH;
-import static com.kakdela.p2p.shared.termux.TermuxConstants.TERMUX_STAGING_PREFIX_DIR;
-import static com.kakdela.p2p.shared.termux.TermuxConstants.TERMUX_STAGING_PREFIX_DIR_PATH;
+import static com.termux.shared.termux.TermuxConstants.TERMUX_PREFIX_DIR;
+import static com.termux.shared.termux.TermuxConstants.TERMUX_PREFIX_DIR_PATH;
+import static com.termux.shared.termux.TermuxConstants.TERMUX_STAGING_PREFIX_DIR;
+import static com.termux.shared.termux.TermuxConstants.TERMUX_STAGING_PREFIX_DIR_PATH;
 
 /**
  * Install the Termux bootstrap packages if necessary by following the below steps:
